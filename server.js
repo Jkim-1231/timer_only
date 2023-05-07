aeed54ddc14d91c747f83cf76c654700587df9f0
木村仁 <kimurajin@kimurahitoshinoMacBook-Air.local>
2023-05-07 12:55:27 +0900

    gitig

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+io.on("connection", (socket) => {
+  console.log("ユーザーが接続しました");
+
+  socket.on('startTimer', () => {
+    let time = 300;
+
+    const timer = setInterval(() => {
+      time--;
+      const minutes = Math.floor(time / 60)
+        .toString()
+        .padStart(2, '0');
+      const seconds = (time % 60).toString().padStart(2, '0');
+      const currentTime = `${minutes}:${seconds}`;
+      io.emit('updateTimer', currentTime);
+
+      if (time <= 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  });
+  
+});
+
+
+
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("listenin on 3000");
+});
